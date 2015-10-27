2c8a445884fa729c35f702318b2328afcd1d96f6
Riley Hooper <studev18@ldsbc.edu>
2015-10-27 16:45:17 -0600

    Added Push code.

---
+++ BC ToolsMultipleDetailViews/AppDelegate.swift
@@ -0,0 +1,120 @@
+//
+//  AppDelegate.swift
+//  LDSBC_Tools
+//
+//  Created by Riley Hooper on 5/13/15.
+//  Copyright (c) 2015 LDS BC. All rights reserved.
+//
+
+import UIKit
+import Parse
+import ParseCrashReporting
+import Bolts
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        // Parse
+        // [Optional] Power your app with Local Datastore. For more info, go to
+        // https://parse.com/docs/ios_guide#localdatastore/iOS
+        //Parse.enableLocalDatastore()
+        
+        // Initialize Parse.
+        ParseCrashReporting.enable()
+        Parse.setApplicationId("y4mrxs1MVgSv9qv5L79fui8d4ElGs7iQH6mJOWha",
+            clientKey: "9Y6aanyHQdDjxzpx6QYsBYxgMUb8tfFsB95caB3c")
+        
+        // [Optional] Track statistics around application opens.
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+        
+        //        // Manually crash the app after 5 seconds.
+        //        let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(5 * Double(NSEC_PER_SEC)))
+        //        dispatch_after(delayTime, dispatch_get_main_queue()) {
+        //            NSException.raise(NSGenericException, format: "Everything is ok, this is just a test crash.", arguments: getVaList([]))
+        //        }
+        
+        //  Push
+        // Register for Push Notitications
+        if application.applicationState != UIApplicationState.Background {
+            // Track an app open here if we launch with a push, unless
+            // "content_available" was used to trigger a background push (introduced in iOS 7).
+            // In that case, we skip tracking here to avoid double counting the app-open.
+            
+            let preBackgroundPush = !application.respondsToSelector("backgroundRefreshStatus")
+            let oldPushHandlerOnly = !self.respondsToSelector("application:didReceiveRemoteNotification:fetchCompletionHandler:")
+            var pushPayload = false
+            if let options = launchOptions {
+                pushPayload = options[UIApplicationLaunchOptionsRemoteNotificationKey] != nil
+            }
+            if (preBackgroundPush || oldPushHandlerOnly || pushPayload) {
+                PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+            }
+        }
+        if application.respondsToSelector("registerUserNotificationSettings:") {
+            let userNotificationTypes = UIUserNotificationType([.Alert, .Badge, .Sound])
+            let settings = UIUserNotificationSettings(forTypes: userNotificationTypes, categories: nil)
+            application.registerUserNotificationSettings(settings)
+            application.registerForRemoteNotifications()
+        } else {
+            let types = UIRemoteNotificationType([.Badge, .Alert, .Sound])
+            application.registerForRemoteNotificationTypes(types)
+        }
+        
+        // Override point for customization after application launch.
+        let splitViewController = self.window!.rootViewController as! UISplitViewController
+        let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as! UINavigationController
+//        if #available(iOS 8.0, *) {
+            navigationController.topViewController!.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem()
+//        } else {
+//            // Fallback on earlier versions
+//        }
+        splitViewController.delegate = self
+        return true
+    }
+    
+    
+    // Store the device token and handle the UI for notifications by adding the following to your main app delegate:
+    func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
+        let installation = PFInstallation.currentInstallation()
+        installation.setDeviceTokenFromData(deviceToken)
+        installation.saveInBackground()
+    }
+    
+    func application(application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError) {
+        if error.code == 3010 {
+            print("Push notifications are not supported in the iOS Simulator.")
+        } else {
+            print("application:didFailToRegisterForRemoteNotificationsWithError: %@", error)
+        }
+    }
+    
+    func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
+        PFPush.handlePush(userInfo)
+        if application.applicationState == UIApplicationState.Inactive {
+            PFAnalytics.trackAppOpenedWithRemoteNotificationPayload(userInfo)
+        }
+    }
+
+   
+    // MARK: - Split view
+
+    func splitViewController(splitViewController: UISplitViewController, collapseSecondaryViewController secondaryViewController:UIViewController, ontoPrimaryViewController primaryViewController:UIViewController) -> Bool {
+        if let secondaryAsNavController = secondaryViewController as? UINavigationController {
+            if let topAsDetailController = secondaryAsNavController.topViewController as? DetailViewController {
+                if topAsDetailController.detailItem == nil {
+                    // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
+                    //If we don't do this, detail1 will open as the first view when run on iPhone, comment and see
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
+}
+
